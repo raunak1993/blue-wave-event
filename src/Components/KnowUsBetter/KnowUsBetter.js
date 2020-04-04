8c9583061e820fe2156380d68fe31214058e74fe
debashis debnath <debashishrambhola@gmail.com>
2020-04-04 19:46:57 +0530

    team page version 1.0

---
+++ src/Components/KnowUsBetter/KnowUsBetter.js
@@ -0,0 +1,16 @@
+import  React from "react";
+import styles from "./KnowUsBetter.module.css";
+const KnowUsBetter = props =>{
+    return (
+        <div className = {styles.KnowUsBetter}>
+        <div className = {styles.heading}>Know Us Better</div>
+        <p className = {styles.knowusbetterpara}>We specialize in unique,
+         tailor-made event layouts that cater to a wide spectrum of events for big corporate names, 
+         private and social programs and reputed educational institutes.
+         All loyal patrons and our widespread clientele, 
+         not only in Kolkata but also in India, 
+         are proof enough for the professional level of sophisticated expertise we bring to every event.</p>
+        </div>
+    )
+}
+export default KnowUsBetter;
