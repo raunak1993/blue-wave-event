367ed9c5f2f58f170e33cd181147d055a4d8de9e
debashis debnath <debashishrambhola@gmail.com>
2020-04-17 19:22:10 +0530

    call button modified

--- src/Components/CallUs/CallUs.js
+++ src/Components/CallUs/CallUs.js
@@ -2,7 +2,7 @@ import React from "react";
 import styles from "./CallUs.module.css"
 const CallUs = () =>{
     return (<>
-    <div className = {styles.callUs}>Call us</div>
+    <a href = "tell:+91123"><div className = {styles.CallUs}><i class = "fa fa-phone fa-3x"></i>Call Now</div></a>
     </>)
 }
 export default CallUs;
